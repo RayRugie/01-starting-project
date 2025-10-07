655c7f5f424eb635de9e447d7805fe1cb304623f
Omorugie Oyibotha <66238145+RayRugie@users.noreply.github.com>
2025-10-07 16:36:31 +0100

    Refactor news components: implement NewsList for news pages and archive, remove unused LatestNewsPage

--- app/archive/@archive/[year]/page.js
+++ app/archive/@archive/[year]/page.js
@@ -1,3 +1,10 @@
-export default function FilteredNewsPage() {
-        
+import NewsList from "@/components/news-list";
+import { getNewsForYear } from "@/lib/news";
+
+export default function FilteredNewsPage({params}) {
+    const newsYear = params.year;
+    const news = getNewsForYear(newsYear)
+    return (
+        <NewsList news={news}/>
+    )
 }
